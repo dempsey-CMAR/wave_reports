7c64c15c1cd01acdd86a9a0f0ebba05b00143478
Danielle Dempsey <ddempsey@perennia.ca>
2024-11-18 11:54:55 -0400

    initial commit

---
+++ 2_generate_wave_reports.R
@@ -0,0 +1,48 @@
+# This script generates reports for all deployments in xxx
+
+# SECTION 1: Read in document history and report files
+
+# SECTION 2: Specify xxxx for which to generate reports
+
+# SECTION 2: Generate report(s)
+
+# SECTION 1: SET UP ---------------------------------------------
+
+library(dplyr)
+library(here)
+library(readxl)
+library(waves)
+
+doc_hist <- read_excel(
+  "R:/tracking_sheets/wave_report_tracker.xlsx", sheet = "Tracking"
+) %>%
+  mutate(Date = as.character(Date)) %>%
+  select(Depl_ID, Version, Date, Amendments)
+
+report <- here("2_wave_report_template2.Rmd")
+
+# SECTION 1: SET UP ---------------------------------------------
+
+county <- "annapolis"
+
+depls <- list.files(
+  paste0("R:/data_branches/wave/processed_data/deployment_data/", county),
+  pattern = ".rds",
+  full.names = TRUE
+) %>%
+  wv_extract_deployment_info2()
+depls <- depls$deployment_id[1]
+
+# SECTION 2: GENERATE REPORTS --------------------------------------------------------
+
+sapply(depls, function(x) {
+
+  rmarkdown::render(
+    input = report,
+    output_file = paste0(x, "_wave_report.docx"),
+    params = list(
+      depl_id = x,
+      doc_hist = filter(doc_hist, Depl_ID == x))
+  )
+})
+
---
+++ 3_assemble_wave_dataset_template.R
@@ -0,0 +1,118 @@
+# DATE:
+# NAME:
+# NOTES:
+
+# Returns assembled data as a csv file in /open_data
+# and as an rds file in /assembled
+
+# SECTION 1: Define county
+# Modify county variable to include the county for which to assemble data
+
+# SECTION 2: Check csv file and rds file are identical
+
+# SECTION 3: Check for duplicate TIMESTAMP
+
+library(data.table)
+library(dplyr)
+library(lubridate)
+library(purrr)
+
+# SECTION 1: Define counties and submission date --------------------------
+
+county <- "annapolis"
+file_date <- as.character(Sys.Date())
+
+path <- file.path("R:/data_branches/wave")
+
+# print Warning if there are files in the /new folder
+dat_new <- list.files(
+  paste0(path, "/processed_data/deployment_data/", county, "/new"), pattern = "rds") %>%
+  unlist()
+
+if(length(dat_new) > 0) {
+  warning(paste0("There are ", length(dat_new), " files in the /new folder.
+               \nMove these to the county folder to assemble"))
+}
+
+# Assemble county dataset ------------------------------------------------
+
+# Import data
+dat_raw <- list.files(
+  paste0(path, "/processed_data/deployment_data/", county),
+  pattern = "rds",
+  full.names = TRUE
+) %>%
+  unlist() %>%
+  map_dfr(readRDS) %>%
+  select(-trim_obs)
+
+# open data portal (summary flags) -------------------------------------------
+# remove the qc_test_variable columns (leaving only the max flag col)
+keep_cols <- c(
+  "waterbody", "station", "deployment_id", "timestamp_utc",
+  "sea_surface_wave_significant_height_m",
+  "sea_surface_wave_peak_period_s", "sea_surface_wave_to_direction_degree",
+  "sea_water_speed_m_s", "sea_water_to_direction_degree",
+  "sensor_depth_below_surface_m",
+  "qc_flag_sea_surface_wave_significant_height_m",
+  "qc_flag_sea_surface_wave_peak_period_s",
+  "qc_flag_sea_surface_wave_to_direction_degree",
+  "qc_flag_sea_surface_wave_sea_water_speed_m_s",
+  "qc_flag_sea_surface_wave_sea_water_to_direction_degree",
+  "qc_flag_sea_surface_wave_sensor_depth_below_surface_m"
+)
+
+dat_raw %>%
+  select(all_of(keep_cols)) %>%
+  qaqcmar::qc_assign_flag_labels() %>%
+  ss_export_county_files(
+    county = county,
+    output_path = paste0(path, "/open_data/"),
+    export_rds = FALSE
+  )
+
+# cmar county data (all flags) --------------------------------------------
+
+# remove columns that are all NA
+dat_raw %>%
+  select_if(~ !all(is.na(.))) %>%
+  ss_export_county_files(
+    county = county,
+    output_path = paste0(path, "/processed_data/assembled/"),
+    export_csv = FALSE
+  )
+
+
+
+
+# Make csv and rds files
+file_name <- paste(county, file_date, sep = "_")
+
+dat_raw %>%
+  mutate(timestamp_utc = as.character(timestamp_utc)) %>%
+  fwrite(file = paste0(path, "/open_data/submitted_data/", file_name, ".csv"))
+
+saveRDS(dat_raw, file = paste0(path, "/processed_data/assembled_data/", file_name, ".rds"))
+
+# SECTION 2: Check csv file and rds file are identical --------------------
+
+dat_csv <- fread(paste0(path, "/open_data/submitted_data/", file_name, ".csv"))
+
+dat_rds <- readRDS(paste0(path, "/processed_data/assembled_data/", file_name, ".rds"))
+
+tz(dat_csv$timestamp_utc)
+tz(dat_rds$timestamp_utc)
+
+all.equal(dat_csv, dat_rds)
+
+# SECTION 3: Check for duplicate TIMESTAMP --------------------
+
+dat_rds %>%
+  select(-sea_water_speed_m_s, -sea_water_to_direction_degree) %>%
+  group_by(
+    waterbody, station, deployment_id, timestamp_utc, sensor_depth_below_surface_m,
+    bin_height_above_sea_floor_m) %>%
+  filter(n() > 1) %>%
+  ungroup() %>%
+  summarize(n_dups = n())
+
